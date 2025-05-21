80c38309d9d375f2651493a5b615261708a832ae
Niveaditha VR <123547068+Niveaditha@users.noreply.github.com>
2025-05-21 11:34:41 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/project2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.project2  // This should match AndroidManifest.xml
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/build.gradle.kts
@@ -0,0 +1,36 @@
+import org.gradle.api.tasks.Delete
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.0.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+val newBuildDir = rootProject.layout.buildDirectory.dir("../../build").get()
+rootProject.layout.buildDirectory.set(newBuildDir)
+
+subprojects {
+    val newSubprojectBuildDir = newBuildDir.dir(project.name)
+    project.layout.buildDirectory.set(newSubprojectBuildDir)
+}
+
+subprojects {
+    evaluationDependsOn(":app")
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
+}
